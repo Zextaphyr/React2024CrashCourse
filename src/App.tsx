c7f8da393e748fdba25c58c461572f501a6a0256
Ícaro Glauco de Oliveira <icaroglauco@proton.me>
2024-11-24 01:22:04 -0300

    update: exercise of name: List of Things I am Excited about learning React in this course

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router";
 import StepOne from "./exercicios/StepOne.tsx"
+import ThingsIamExcitedAboutReact from "./exercicios/ThingsExcitedAboutReact.tsx"
 
 function App() {
   return (
    <BrowserRouter>
       <Routes>
         <Route index element={<StepOne />} />
+        <Route path="/ThingsIamExcitedAbout" element={<ThingsIamExcitedAboutReact/>}></Route> 
       </Routes>  
     </BrowserRouter>
   )
---
+++ src/exercicios/ThingsExcitedAboutReact.tsx
@@ -0,0 +1,10 @@
+export default function ThingsExcitedAbout() {
+  return <>
+    <h2>List of Things I am Excited about learning React in this course</h2>
+    <ul>
+    <li>Being up to date with web creative development of useful virtual solutions</li>
+    <li>Create a new curriculum with it</li>
+    <li>Get rid of insecurity</li>
+  </ul> 
+  </> 
+}
