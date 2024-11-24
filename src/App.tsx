a44f856748f192a790e746ff2eb987452ad6719e
Ícaro Glauco de Oliveira <icaroglauco@proton.me>
2024-11-24 02:21:27 -0300

    working setup with vitejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router";
+
 import StepOne from "./exercicios/StepOne.tsx"
 import ThingsIamExcitedAboutReact from "./exercicios/ThingsExcitedAboutReact.tsx"
+import ReactFunFacts from "./challenges/ReactFunFacts.tsx"
+
 
 function App() {
   return (
@@ -10,6 +13,7 @@ function App() {
       <Routes>
         <Route index element={<StepOne />} />
         <Route path="/ThingsIamExcitedAbout" element={<ThingsIamExcitedAboutReact/>}></Route> 
+        <Route path="/ReactFunFacts" element={<ReactFunFacts/>}/>
       </Routes>  
     </BrowserRouter>
   )
---
+++ src/challenges/ReactFunFacts.tsx
@@ -0,0 +1,35 @@
+// React fun facts:
+// uma aplicação introdutória de react para uma página simples de lista de fatos sobre react
+//
+import ReactLogo from '../assets/react.svg';
+import "./style.css";
+import {Fragment} from 'react'
+
+export default function ReactFunFacts () {
+  return <Fragment>
+          <Header/>
+          <MainContent/>
+          <Footer/>
+        </Fragment>
+}
+
+const Header = () => <header>
+    <img src={ReactLogo} id="ReactLogo"/>
+    </header>
+
+const MainContent = () => <main>
+      <h1>Reasons I am excited to keeping using React !</h1>
+      <ol>
+        <li>Was first released in 2013</li>
+        <li>Criado por Jordan Walke</li>
+        <li> Na verdade pagou minha viagem para europa haha</li>
+
+      </ol>
+    </main>   
+ 
+const Footer = () => <footer>
+      <small>
+© 2024 Zextaphyr development. Todos os direitos reservados
+      </small>      
+    </footer>
+
